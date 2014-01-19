db9e67e1dcd4cc2847c27db07a1c31718a51cac7
Gerald McAlister <gerald.mcalister.gem@gmail.com>
2014-01-19 12:29:37 -0500

    Initial Commit
    Contains a quick sample thrown together for viewing up to 4 Microsoft
    Kinect color image streams.

---
+++ Multi Kinect Sample/Multi Kinect Sample/mainForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Multi_Kinect_Sample
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.kinectPB1 = new System.Windows.Forms.PictureBox();
+            this.kinectPB2 = new System.Windows.Forms.PictureBox();
+            this.kinectPB4 = new System.Windows.Forms.PictureBox();
+            this.kinectPB3 = new System.Windows.Forms.PictureBox();
+            this.createdByLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.kinectPB1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kinectPB2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kinectPB4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kinectPB3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // kinectPB1
+            // 
+            this.kinectPB1.Location = new System.Drawing.Point(12, 12);
+            this.kinectPB1.Name = "kinectPB1";
+            this.kinectPB1.Size = new System.Drawing.Size(300, 200);
+            this.kinectPB1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.kinectPB1.TabIndex = 0;
+            this.kinectPB1.TabStop = false;
+            // 
+            // kinectPB2
+            // 
+            this.kinectPB2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.kinectPB2.Location = new System.Drawing.Point(312, 12);
+            this.kinectPB2.Name = "kinectPB2";
+            this.kinectPB2.Size = new System.Drawing.Size(300, 200);
+            this.kinectPB2.TabIndex = 1;
+            this.kinectPB2.TabStop = false;
+            // 
+            // kinectPB4
+            // 
+            this.kinectPB4.Location = new System.Drawing.Point(312, 229);
+            this.kinectPB4.Name = "kinectPB4";
+            this.kinectPB4.Size = new System.Drawing.Size(300, 200);
+            this.kinectPB4.TabIndex = 3;
+            this.kinectPB4.TabStop = false;
+            // 
+            // kinectPB3
+            // 
+            this.kinectPB3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.kinectPB3.Location = new System.Drawing.Point(12, 229);
+            this.kinectPB3.Name = "kinectPB3";
+            this.kinectPB3.Size = new System.Drawing.Size(300, 200);
+            this.kinectPB3.TabIndex = 2;
+            this.kinectPB3.TabStop = false;
+            // 
+            // createdByLabel
+            // 
+            this.createdByLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.createdByLabel.AutoSize = true;
+            this.createdByLabel.Location = new System.Drawing.Point(242, 215);
+            this.createdByLabel.Name = "createdByLabel";
+            this.createdByLabel.Size = new System.Drawing.Size(141, 13);
+            this.createdByLabel.TabIndex = 4;
+            this.createdByLabel.Text = "Created by: Gerald McAlister";
+            this.createdByLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.createdByLabel);
+            this.Controls.Add(this.kinectPB4);
+            this.Controls.Add(this.kinectPB3);
+            this.Controls.Add(this.kinectPB2);
+            this.Controls.Add(this.kinectPB1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "mainForm";
+            this.Text = "Multi Kinect Demo";
+            this.Load += new System.EventHandler(this.mainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.kinectPB1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kinectPB2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kinectPB4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kinectPB3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox kinectPB1;
+        private System.Windows.Forms.PictureBox kinectPB2;
+        private System.Windows.Forms.PictureBox kinectPB4;
+        private System.Windows.Forms.PictureBox kinectPB3;
+        private System.Windows.Forms.Label createdByLabel;
+    }
+}
+
---
+++ Multi Kinect Sample/Multi Kinect Sample/mainForm.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Kinect;
+using System.Drawing.Imaging;
+
+namespace Multi_Kinect_Sample
+{
+    public partial class mainForm : Form
+    {
+        List<KinectSensor> kinects;
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+            int kinectCount = 0;
+            kinects = new List<KinectSensor>();
+            foreach (KinectSensor kinect in KinectSensor.KinectSensors)
+            {
+                switch (kinectCount)
+                {
+                    case 0:
+                        kinect.ColorFrameReady += new EventHandler<ColorImageFrameReadyEventArgs>(mainForm_ColorFrameReady1);
+                        break;
+                    case 1:
+                        kinect.ColorFrameReady += new EventHandler<ColorImageFrameReadyEventArgs>(mainForm_ColorFrameReady2);
+                        break;
+                    case 2:
+                        kinect.ColorFrameReady += new EventHandler<ColorImageFrameReadyEventArgs>(mainForm_ColorFrameReady3);
+                        break;
+                    case 3:
+                        kinect.ColorFrameReady += new EventHandler<ColorImageFrameReadyEventArgs>(mainForm_ColorFrameReady4);
+                        break;
+                }
+                kinect.DepthStream.Disable();
+                kinect.SkeletonStream.Disable();
+                // Enable the color stream at 640x480 resolution and 30 frames per second.
+                kinect.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+                // Start the Kinect camera.
+                kinect.Start();
+                kinects.Add(kinect);
+
+                kinectCount += 1;
+            }
+        }
+
+        void setImage(int imageIndex, ColorImageFrame frame)
+        {
+            // Check that the frame is not empty.
+            if (frame != null)
+            {
+                // Create an array of bytes to hold the data.
+                byte[] pixelData = new byte[frame.PixelDataLength];
+                // Copy the pixel data to the array.
+                frame.CopyPixelDataTo(pixelData);
+
+                // Create a bmp to hold the actual image.
+                Bitmap bmp = new Bitmap(frame.Width, frame.Height, PixelFormat.Format32bppRgb);
+                // Create the bitmap data to hold the data for the image.
+                BitmapData bmpData = bmp.LockBits(new Rectangle(0, 0, frame.Width, frame.Height), ImageLockMode.WriteOnly, bmp.PixelFormat);
+                // Create the pointer to the bmp data.
+                IntPtr ptr = bmpData.Scan0;
+                // Then copy the actual data to the bmp.
+                System.Runtime.InteropServices.Marshal.Copy(pixelData, 0, ptr, frame.PixelDataLength);
+                // Unlock the bmp data bits so they can be used.
+                bmp.UnlockBits(bmpData);
+                switch (imageIndex)
+                {
+                    case 0:
+                        // Set the color image to the bmp image.
+                        this.kinectPB1.Image = bmp;
+                        break;
+                    case 1:
+                        // Set the color image to the bmp image.
+                        this.kinectPB2.Image = bmp;
+                        break;
+                    case 2:
+                        // Set the color image to the bmp image.
+                        this.kinectPB3.Image = bmp;
+                        break;
+                    case 3:
+                        // Set the color image to the bmp image.
+                        this.kinectPB4.Image = bmp;
+                        break;
+                }
+
+                // Dispose of the frame.
+                frame.Dispose();
+            }
+        }
+
+        void mainForm_ColorFrameReady(int imageIndex, object sender, ColorImageFrameReadyEventArgs e)
+        {
+            // Create a color frame to hold the color frame that was recieved.
+            ColorImageFrame frame = e.OpenColorImageFrame();
+            setImage(imageIndex, frame);
+        }
+
+        void mainForm_ColorFrameReady1(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            mainForm_ColorFrameReady(0, sender, e);
+        }
+        void mainForm_ColorFrameReady2(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            mainForm_ColorFrameReady(1, sender, e);
+        }
+        void mainForm_ColorFrameReady3(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            mainForm_ColorFrameReady(2, sender, e);
+        }
+        void mainForm_ColorFrameReady4(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            mainForm_ColorFrameReady(3, sender, e);
+        }
+    }
+}
